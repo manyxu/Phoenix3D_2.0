cf4310c5f00c19ca2cabc798b373e4d7a31d8d78
xuduo <realmany@163.com>
2012-11-24 02:14:12 +0800

    PreViewWindow

--- Phoenix2p1/Tools/Editor/PX2PreViewWindow.cpp
+++ Phoenix2p1/Tools/Editor/PX2PreViewWindow.cpp
@@ -8,9 +8,13 @@
 using namespace PX2Editor;
 using namespace PX2;
 
+//----------------------------------------------------------------------------
+#define ID_PREVIEWTIMERR wxID_HIGHEST+3501
+//----------------------------------------------------------------------------
 IMPLEMENT_DYNAMIC_CLASS(PX2Editor::PreViewWindow, wxWindow)
 BEGIN_EVENT_TABLE(PreViewWindow, wxWindow)
-EVT_PAINT(PreViewWindow::OnPaint)
+	EVT_TIMER(ID_PREVIEWTIMERR, PreViewWindow::OnTimer)
+	EVT_PAINT(PreViewWindow::OnPaint)
 END_EVENT_TABLE()
 //----------------------------------------------------------------------------
 PreViewWindow::PreViewWindow ()
@@ -20,7 +24,8 @@ PreViewWindow::PreViewWindow ()
 //----------------------------------------------------------------------------
 PreViewWindow::PreViewWindow (wxWindow *parent)
 	:
-wxWindow(parent, -1, wxDefaultPosition, wxSize(200, 150))
+wxWindow(parent, -1, wxDefaultPosition, wxSize(200, 150)),
+	mInited(false)
 {
 }
 //----------------------------------------------------------------------------
@@ -28,8 +33,69 @@ PreViewWindow::~PreViewWindow ()
 {
 }
 //----------------------------------------------------------------------------
+void PreViewWindow::OnTimer (wxTimerEvent& event)
+{
+	Update();
+	DrawScene();
+}
+//----------------------------------------------------------------------------
 void PreViewWindow::OnPaint (wxPaintEvent& e)
 {
 	wxPaintDC dc(this);
+}
+//----------------------------------------------------------------------------
+void PreViewWindow::Initialize ()
+{
+	mPreViewTimer.SetOwner(this, ID_PREVIEWTIMERR);
+	mPreViewTimer.Start(50);
+
+	mWidth = 200;
+	mHeight = 150;
+	mColorFormat = Texture::TF_A8R8G8B8;
+	mDepthStencilFormat = Texture::TF_D24S8;
+	mNumMultisamples = 0;
+
+	HWND hWnd = (HWND)(GetHandle());
+	mInput.mWindowHandle = hWnd;
+	mInput.mDriver = Direct3DCreate9(D3D_SDK_VERSION);
+	assertion(mInput.mDriver != 0, "Failed to create Direct3D9\n");
+	mRenderer = new0 Renderer(mInput, mWidth, mHeight, mColorFormat, 
+		mDepthStencilFormat, mNumMultisamples);
+	mBackGroundColor = Float4(0.4f, 0.4f, 0.4f, 0.0f);
+	mRenderer->SetClearColor(mBackGroundColor);
+
+	mCamera = new0 Camera();
+	mPreViewTextureCamera = ScreenTarget::CreateCamera();
+
+	mVFormat = VertexFormat::Create(2,
+		VertexFormat::AU_POSITION, VertexFormat::AT_FLOAT3, 0,
+		VertexFormat::AU_TEXCOORD, VertexFormat::AT_FLOAT2, 0);
+	mScreenMesh = ScreenTarget::CreateRectangle(mVFormat, mWidth, mHeight,
+		0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+	mPreViewMaterial = new Texture2DMaterial();
+}
+//----------------------------------------------------------------------------
+void PreViewWindow::SetTexture (PX2::Texture2D *texture)
+{
+	mPreViewTexture = texture;
+	mScreenMesh->SetMaterialInstance(mPreViewMaterial
+		->CreateInstance(texture));
+
+	mScene = mScreenMesh;
+}
+//----------------------------------------------------------------------------
+void PreViewWindow::SetModel (PX2::Movable *object)
+{
+	mPreViewObject = object;
+	mScene = object;
+}
+//----------------------------------------------------------------------------
+void PreViewWindow::Update ()
+{
+
+}
+//----------------------------------------------------------------------------
+void PreViewWindow::DrawScene ()
+{
 }
 //----------------------------------------------------------------------------
--- Phoenix2p1/Tools/Editor/PX2PreViewWindow.hpp
+++ Phoenix2p1/Tools/Editor/PX2PreViewWindow.hpp
@@ -20,13 +20,49 @@ namespace PX2Editor
 		virtual ~PreViewWindow ();
 
 		DECLARE_DYNAMIC_CLASS(PreViewWindow)
-		
+	
 		void SetObject (PX2::Object *obj);
 
+		void OnTimer (wxTimerEvent& event);
 		void OnPaint(wxPaintEvent& e);
+
+public_internal:
+		void Initialize ();
 
 	protected:
 		DECLARE_EVENT_TABLE()
+
+		void SetTexture (PX2::Texture2D *texture);
+		void SetModel (PX2::Movable *object);
+		void Update ();
+		void DrawScene ();
+
+		bool mInited;
+
+		PX2::RendererInput mInput;
+		PX2::Texture::Format mColorFormat;
+		PX2::Texture::Format mDepthStencilFormat;
+		int mNumMultisamples;
+		PX2::Renderer* mRenderer;
+		PX2::Float4 mBackGroundColor;
+		
+		int mWidth, mHeight;
+		PX2::CameraPtr mCamera;
+		PX2::VertexFormatPtr mVFormat;
+		PX2::TriMeshPtr mScreenMesh;
+		PX2::Texture2DMaterialPtr mPreViewMaterial;
+		PX2::TexturePtr mPreViewTexture;
+		PX2::CameraPtr mPreViewTextureCamera;
+
+		PX2::MovablePtr mPreViewObject;
+
+		// Scene
+		PX2::Culler mCuller;
+		PX2::MovablePtr mScene;
+		PX2::Culler mCuller1;
+		PX2::MovablePtr mScene1;
+
+		wxTimer mPreViewTimer;
 	};
 
 }
